abfeda275593041a67b4769f87fbaa53a5025843
Alexandr <fomin02@inbox.ru>
2022-12-19 09:01:19 +0500

    main-com-9

--- src/js/main.js
+++ src/js/main.js
@@ -135,6 +135,55 @@ settings: {
 ]
 });
 
+$('.gallery2').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  adaptiveHeight: true,
+  prevArrow: false,
+  nextArrow: false,
+  responsive: [
+  {
+  breakpoint: 1024,
+  settings: {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: false,
+    adaptiveHeight: true,
+    prevArrow: false,
+    nextArrow: false,
+    dots: false
+  }
+  },
+  {
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 2,
+    adaptiveHeight: true,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToScroll: 2
+    
+  }
+  },
+  {
+  breakpoint: 480,
+  settings: {
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToScroll: 1
+  }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
+  });
+
 
 $('.slider').slick({
   dots: false,
